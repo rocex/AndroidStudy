068bc6adf9f27cf9e68f27c2ecbf344c7771d27d
rocex <rocexwang@gmail.com>
2017-05-11 16:30:49 +0800

    init

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -9,6 +11,19 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void onOrder(View view)
+    {
+        displayQuantity();
+    }
+
+    private void displayQuantity()
+    {
+        TextView textView = (TextView) findViewById(R.id.textView0);
+
+        textView.setText("200");
     }
 }
