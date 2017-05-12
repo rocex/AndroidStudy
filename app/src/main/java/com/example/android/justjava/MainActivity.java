ef40c5eaf4825fe2106a868727226710e6d19ebb
rocex <rocexwang@gmail.com>
2017-05-12 09:16:42 +0800

    plus

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
-    @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -22,9 +22,13 @@ public class MainActivity extends AppCompatActivity
 
     private void displayQuantity()
     {
+        EditText editTextPrice = (EditText) findViewById(R.id.editTextPrice);
+        EditText editTextCount = (EditText) findViewById(R.id.editTextCount);
         TextView textViewQuantity = (TextView) findViewById(R.id.textViewQuantity);
-        TextView editTextPrice = (TextView) findViewById(R.id.editTextPrice);
 
-        textViewQuantity.setText("200");
+        double dblPrice = Double.parseDouble(editTextPrice.getText().toString());
+        double dblCount = Double.parseDouble(editTextCount.getText().toString());
+
+        textViewQuantity.setText(String.valueOf(dblPrice * dblCount));
     }
 }
