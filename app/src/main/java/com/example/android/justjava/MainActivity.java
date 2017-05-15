8f21378318b79dd35769a72f22710b7e439c7237
rocex <rocexwang@gmail.com>
2017-05-15 15:04:50 +0800

    init

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -13,40 +13,42 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
+        
         setContentView(R.layout.activity_main);
     }
-
+    
     public void onOrder(View view)
     {
         EditText editTextPrice = (EditText) findViewById(R.id.editTextPrice);
         EditText editTextCount = (EditText) findViewById(R.id.editTextCount);
         TextView textViewQuantity = (TextView) findViewById(R.id.textViewQuantity);
-
+        
+        int iCount = Integer.parseInt(editTextCount.getText().toString());
         double dblPrice = Double.parseDouble(editTextPrice.getText().toString());
-        double dblCount = Double.parseDouble(editTextCount.getText().toString());
-
-        textViewQuantity.setText(NumberFormat.getCurrencyInstance().format(dblPrice * dblCount));
+        
+        textViewQuantity.setText(NumberFormat.getCurrencyInstance().format(dblPrice * iCount));
     }
-
+    
     public void onAdd(View view)
     {
         EditText editTextCount = (EditText) findViewById(R.id.editTextCount);
-
+        
         int iCount = Integer.parseInt(editTextCount.getText().toString());
-
+        
         editTextCount.setText(String.valueOf(iCount + 1));
     }
-
+    
     public void onReduce(View view)
     {
         EditText editTextCount = (EditText) findViewById(R.id.editTextCount);
-
+        
         int iCount = Integer.parseInt(editTextCount.getText().toString());
-
-        if (iCount <= 1) return;
-
+        
+        if(iCount <= 1)
+        {
+            return;
+        }
+        
         editTextCount.setText(String.valueOf(iCount - 1));
     }
 }
-
