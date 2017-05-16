d647c60fab0f909229ad56679154e21635db768d
rocex <rocexwang@gmail.com>
2017-05-16 12:28:18 +0800

    test

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -13,16 +13,17 @@ public class MainActivity extends AppCompatActivity
 {
     int iCount = 1;
     
+    
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-    
+        
         setContentView(R.layout.activity_main);
-    
+        
         EditText editTextCount = (EditText) findViewById(R.id.editTextCount);
-    
+        
         iCount = Integer.parseInt(editTextCount.getText().toString());
-    
+        
         EditText editTextPrice = (EditText) findViewById(R.id.editTextPrice);
         editTextPrice.setText(NumberFormat.getCurrencyInstance().format(Double.parseDouble(editTextPrice.getText().toString())));
     }
@@ -30,12 +31,12 @@ public class MainActivity extends AppCompatActivity
     public void submitOrder(View view)
     {
         Log.d(getClass().getSimpleName(), "enter submitOrder function.");
-    
+        
         EditText editTextPrice = (EditText) findViewById(R.id.editTextPrice);
         TextView textViewQuantity = (TextView) findViewById(R.id.textViewQuantity);
-    
+        
         double dblPrice = Double.parseDouble(editTextPrice.getText().toString());
-    
+        
         textViewQuantity.setText(NumberFormat.getCurrencyInstance().format(dblPrice * iCount));
     }
     
@@ -50,7 +51,7 @@ public class MainActivity extends AppCompatActivity
         {
             return;
         }
-    
+        
         displayCount(--iCount);
     }
     
