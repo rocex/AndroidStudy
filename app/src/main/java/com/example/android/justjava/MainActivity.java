4f8bddb5d90aae3e8c72158fe50cdf0a5b9072a3
rocex <rocexwang@gmail.com>
2017-05-16 12:31:14 +0800

    reset

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity
 {
     int iCount = 1;
     
-    
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
