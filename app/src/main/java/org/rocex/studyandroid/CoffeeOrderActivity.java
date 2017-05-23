b2fe1dbf1a8c247b0cd8e123039ea55adbf049bb
rocex <rocexwang@gmail.com>
2017-05-23 14:17:38 +0800

    重构，和教程一致

--- app/src/main/java/org/rocex/studyandroid/CoffeeOrderActivity.java
+++ app/src/main/java/org/rocex/studyandroid/CoffeeOrderActivity.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.MessageFormat;
 import java.text.NumberFormat;
 
 public class CoffeeOrderActivity extends AppCompatActivity
 {
     int iCount = 1;
+    double dblPrice = 0;
     
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -25,27 +28,37 @@ public class CoffeeOrderActivity extends AppCompatActivity
     
     private void displayOrderSummary()
     {
+        CheckBox checkboxChocolate = (CheckBox) findViewById(R.id.checkboxChocolate);
+        CheckBox checkboxWhippedCream = (CheckBox) findViewById(R.id.checkboxWhippedCream);
+    
+        EditText editTextPrice = (EditText) findViewById(R.id.editTextPrice);
+    
+        dblPrice = Double.parseDouble(editTextPrice.getText().toString());
+    
+        String strSummary = MessageFormat.format("Name:{0}\nAdd Whipped cream: {1}\nAdd Chocolate: {2}\nQuantity: {3}\nTotal: {4}\n\nThank you!"
+                , "Lyla", checkboxWhippedCream.isChecked(), checkboxChocolate.isChecked(), iCount, NumberFormat.getCurrencyInstance().format(dblPrice * iCount));
+        
         TextView textViewSummary = (TextView) findViewById(R.id.textViewSummary);
-        textViewSummary.setText("test");
+        textViewSummary.setText(strSummary);
     }
     
     public void submitOrder(View view)
     {
         Log.d(getClass().getSimpleName(), "enter submitOrder function.");
     
-        displayQuantity();
-    
         displayOrderSummary();
     }
     
-    private void displayQuantity()
+    public void resetOrder(View view)
     {
-        EditText editTextPrice = (EditText) findViewById(R.id.editTextPrice);
-        TextView textViewQuantity = (TextView) findViewById(R.id.textViewQuantity);
-        
-        double dblPrice = Double.parseDouble(editTextPrice.getText().toString());
-        
-        textViewQuantity.setText(NumberFormat.getCurrencyInstance().format(dblPrice * iCount));
+        iCount = 1;
+        dblPrice = 10;
+    
+        displayCount(iCount);
+        displayPrice(dblPrice);
+    
+        TextView textViewSummary = (TextView) findViewById(R.id.textViewSummary);
+        textViewSummary.setText("");
     }
     
     public void onIncrementCount(View view)
@@ -68,5 +81,12 @@ public class CoffeeOrderActivity extends AppCompatActivity
         TextView editTextCount = (TextView) findViewById(R.id.editTextCount);
     
         editTextCount.setText(String.valueOf(iNewCount));
+    }
+    
+    private void displayPrice(double dblNewPrice)
+    {
+        EditText editTextPrice = (EditText) findViewById(R.id.editTextPrice);
+        
+        editTextPrice.setText(String.valueOf(dblNewPrice));
     }
 }
